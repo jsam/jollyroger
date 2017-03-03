3dca4aceb07be5e45506ed54253d562d5f941deb
sam <psam.dev@gmail.com>
2017-03-03 10:57:17 +0100

    Cleanup

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ from distutils.core import setup
 setup(name='jollyroger',
       version='0.0.1',
       description='Kademlia DHT implementation for distributed computing.',
-      author='SamEpps',
-      author_email='samepps.dev@gmail.com',
+      author='Sam Picek',
+      author_email='psam.dev@gmail.com',
       packages=['jdht', 'jdht.stun', 'jdht.tasks'])  #'src.stun', 'src.tasks', 'src.tests', 'src.tftpy', 'src.upnp'])
